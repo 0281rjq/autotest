9aeabc50b51d3f30c34a1b8af5f605e4558d87e5
0281rjq <1553438395@qq.com>
2020-05-23 23:35:25 +0800

    all autotest file

---
+++ src/baidu_ui/const.py
@@ -0,0 +1,2 @@
+#conding=utf-8
+EXCEL_DIR="D:\课件\大三下\移动测试\ceshi\pytest_demo\data\search_query.xlsx"
---
+++ src/httpbin_api/const.py
@@ -0,0 +1,5 @@
+﻿#coding=utf-8
+BASE_URL="http://httpbin.org/"
+IP_URL="/ip"
+LOCAL_IP="118.120.67.124"
+POST_TEST_URL="/post"
